d18335561c7c6b69b4f6cc2af1d44c7df1bf1bcd
camwoodward3 <camwoodward3@icloud.com>
2024-12-06 12:08:41 -0700

    story changes

--- js/story_template.mjs
+++ js/story_template.mjs
@@ -1,1 +1,25 @@
 // Please create the story template in here and export for use.
+
+function lightStoryTemplate(story) {
+    return `
+    <h2>Character Story: <span id="character-menu">${story.characterName}</span></h2>
+    <p id="story-intro>${story.intro}</p>
+    <p id="story-part1>${story.part1}</p>
+    <div class="choices">
+        <button id="choice-1" onclick="${story.loyal_onclick}()">${story.loyal_choice}</button>
+        <button id="choice-2" onclick="${story.dark_onclick}()">${story.dark_choice}</button>
+    </div>
+    <div id="loyal-story" class="story-path>
+        <p id="loyal-choice>${story.loyal_path}</p>
+        <p id="ending-good>
+            <strong>Ending:</strong>${story.ending_good}
+        </p>
+    </div>
+    <div id="dark-side-story" class="story-path>
+        <p id="dark-choice>${story.dark_path}</p>
+        <p id="ending-bad>
+            <strong>Ending:</strong>${story.ending_bad}
+        </p>
+    </div>`
+    
+}
